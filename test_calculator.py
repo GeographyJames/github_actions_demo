88ff002e01ba93016ae457f97cb74d0fcac09018
James Campbell <james.e.campbell@outlook.com>
2024-02-09 15:17:27 +0000

    Added failing subtraction test

--- test_calculator.py
+++ test_calculator.py
@@ -4,3 +4,6 @@ class TestCalculator:
     
     def test_should_perform_addition(self):
         assert calculator.add(1, 2) == 3
+
+    def test_should_perform_subtraction(self):
+        assert calculator.subtract(2, 1) == 1
